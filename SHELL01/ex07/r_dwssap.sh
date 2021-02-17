bf47918a64de09b35342529d19507e0831bcbcf0
seeys <es2008e@naver.com>
2021-02-17 23:13:00 +0900

    ex07

--- SHELL01/ex07/r_dwssap.sh
+++ SHELL01/ex07/r_dwssap.sh
@@ -1,1 +1,1 @@
-cat /etc/passwd | sed '/#/d' | awk '{if (NR%2==0) print}' | sed 's/:.*//' | rev | sort -r | sed -n "${FT_LINE1}${FT_LINE2}p" | tr '\n' ',' | sed 's/,/, /g' | sed 's/, $/./' | tr -d '\n'
+cat /etc/passwd | sed '/#/d' | sed '1d' | sed 's/:.*//' | rev | sort -r | sed -n "${FT_LINE1},${FT_LINE2}p" | tr '\n' ',' | sed 's/,/, /g' | sed 's/, $/./' | tr -d '\n'

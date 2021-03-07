ecaf105c0410b4140642d3c345457199f3393f8f
seeys <es2008e@naver.com>
2021-03-07 15:36:09 +0900

    fin

--- C02/ex11/ft_putstr_non_printable.c
+++ C02/ex11/ft_putstr_non_printable.c
@@ -6,7 +6,7 @@
 /*   By: seehan <seehan@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/25 22:47:32 by seehan            #+#    #+#             */
-/*   Updated: 2021/02/25 23:30:50 by seehan           ###   ########.fr       */
+/*   Updated: 2021/03/07 15:30:51 by seehan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,23 +17,21 @@ void	ft_putchar(char c)
 	write(1, &c, 1);
 }
 
-void	is_not_word(char *str)
+void	is_not_word(unsigned char str)
 {
-	char hex;
-	
-	hex = "0123456789abcdef";
-
+	write(1, "\\", 1);
+	ft_putchar("0123456789abcdef"[str / 16]);
+	ft_putchar("0123456789abcdef"[str % 16]);
 }
+
 void	ft_putstr_non_printable(char *str)
 {
 	while (*str)
 	{
-		if ((*str >= 0 && *str <= 32) || *str == 127)
+		if (*str < 32 || *str > 126)
 			is_not_word(*str);
 		else
-		{
 			write(1, str, 1);
-		}
 		str++;
+	}
 }
-

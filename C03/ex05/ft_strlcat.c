d2258822354636aba78e4bffe32487f049b6eb64
seeys <es2008e@naver.com>
2021-03-08 17:14:08 +0900

    fin

--- C03/ex05/ft_strlcat.c
+++ C03/ex05/ft_strlcat.c
@@ -6,33 +6,47 @@
 /*   By: seehan <seehan@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/06 17:26:31 by seehan            #+#    #+#             */
-/*   Updated: 2021/03/06 17:30:43 by seehan           ###   ########.fr       */
+/*   Updated: 2021/03/08 17:13:58 by seehan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+#include <string.h>
+
 unsigned	int		ft_strlcat(char *dest, char *src, unsigned int size)
 {
+	unsigned int d_len;
+	unsigned int result;
 	unsigned int i;
 
+	d_len = 0;
+	while (dest[d_len] != '\0')
+		d_len++;
+	result = 0;
+	while (src[result])
+		result++;
+	if (size <= d_len)
+		result = result + size;
+	else
+		result = result + d_len;
 	i = 0;
-	while (*dest && i < size)
+	while (src[i] != '\0' && d_len + 1 <size)
 	{
-		i++;
-		dest++;
-	}
-	while (*src && (i + 1) < size)
-	{
-		*dest = *src;
-		dest++;
-		src++;
+		dest[d_len] = src[i];
+		d_len++;
 		i++;
 	}
-	if (i < size)
-		*dest = 0;
-	while (*src)
-	{
-		i++;
-		src++;
-	}
-	return (i);
+	dest[d_len] = '\0';
+	return (result);
+}
+
+int main(void)
+{
+	char s1[5];
+	char s2[4] = "abc";
+	char s3[5];
+	char s4[4] = "abc";
+	printf("%u", ft_strlcat(s1,s2,2));
+	printf("%lu", strlcat(s3,s4,2));
+	return (0);
 }

e8348ba3bf34bb3e0519344bdc4255899e54c2fb
seeys <es2008e@naver.com>
2021-03-08 17:15:49 +0900

    fin

--- C03/ex05/ft_strlcat.c
+++ C03/ex05/ft_strlcat.c
@@ -6,12 +6,9 @@
 /*   By: seehan <seehan@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/06 17:26:31 by seehan            #+#    #+#             */
-/*   Updated: 2021/03/08 17:13:58 by seehan           ###   ########.fr       */
+/*   Updated: 2021/03/08 17:15:37 by seehan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include <stdio.h>
-#include <string.h>
 
 unsigned	int		ft_strlcat(char *dest, char *src, unsigned int size)
 {
@@ -30,7 +27,7 @@ unsigned	int		ft_strlcat(char *dest, char *src, unsigned int size)
 	else
 		result = result + d_len;
 	i = 0;
-	while (src[i] != '\0' && d_len + 1 <size)
+	while (src[i] != '\0' && d_len + 1 < size)
 	{
 		dest[d_len] = src[i];
 		d_len++;
@@ -38,15 +35,4 @@ unsigned	int		ft_strlcat(char *dest, char *src, unsigned int size)
 	}
 	dest[d_len] = '\0';
 	return (result);
-}
-
-int main(void)
-{
-	char s1[5];
-	char s2[4] = "abc";
-	char s3[5];
-	char s4[4] = "abc";
-	printf("%u", ft_strlcat(s1,s2,2));
-	printf("%lu", strlcat(s3,s4,2));
-	return (0);
 }

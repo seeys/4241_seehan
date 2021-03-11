c00cceaadcf331179edd0dca438c86c084afa7be
seeys <es2008e@naver.com>
2021-03-11 21:58:13 +0900

    fin

--- C06/ex00/ft_print_program_name.c
+++ C06/ex00/ft_print_program_name.c
@@ -6,7 +6,7 @@
 /*   By: seehan <seehan@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/09 13:06:04 by seehan            #+#    #+#             */
-/*   Updated: 2021/03/11 19:27:36 by seehan           ###   ########.fr       */
+/*   Updated: 2021/03/11 21:54:11 by seehan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@ int		main(int argc, char **argv)
 	int i;
 
 	i = 0;
+	(void) argc;
 	while (argv[0][i])
 		write(1, &argv[0][i++], 1);
 	return (0);

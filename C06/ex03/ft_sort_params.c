23aa480c4c498615e0ed6e89a0003935b209cec9
seeys <es2008e@naver.com>
2021-03-11 19:28:00 +0900

    fin

---
+++ C06/ex03/ft_sort_params.c
@@ -0,0 +1,72 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_params.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seehan <seehan@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/09 13:36:24 by seehan            #+#    #+#             */
+/*   Updated: 2021/03/09 18:34:19 by seehan           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+int		ft_strcmp(char *s1, char *s2)
+{
+	unsigned int i;
+
+	i = 0;
+	while (s1[i] && s2[i])
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+		i++;
+	}
+	return (s1[i] - s2[i]);
+}
+
+void	sort(int ac, char **av)
+{
+	int i;
+	char *tmp;
+
+	i = 1;
+	while (i < ac - 1)
+	{
+		if (ft_strcmp(av[i], av[i + 1]) > 0)
+		{
+			tmp = av[i];
+			av[i] = av[i + 1];
+			av[i + 1] = tmp;
+		}
+		else
+			i++;
+	}
+}
+
+int		main(int ac, char **av)
+{
+	int i;
+	int j;
+
+	i = 1;
+	sort(ac, av);
+	while (i < ac)
+	{
+		j = 0;
+		while (av[i][j])
+		{
+			ft_putchar(av[i][j]);
+			j++;
+		}
+		i++;
+		write(1, "\n", 1);
+	}
+	return (0);
+}
